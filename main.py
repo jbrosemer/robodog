335ff29ce61eb9ff2cc509ffbef59f6deb8af46a
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-12-13 02:20:38 -0500

    late night :

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import RPi.GPIO as GPIO
+from gpiozero import Servo
 from time import sleep
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-pwm=GPIO.PWM(11, 50)
-pwm.start(0)
-pwm.ChangeDutyCycle(5) # left -90 deg position
-sleep(1)
-pwm.ChangeDutyCycle(7.5) # neutral position
-sleep(1)
-pwm.ChangeDutyCycle(10) # right +90 deg position
-sleep(1)
+
+servo = Servo(17)
+
+while True:
+    servo.min()
+    sleep(1)
+    servo.mid()
+    sleep(1)
+    servo.max()
+    sleep(1)
